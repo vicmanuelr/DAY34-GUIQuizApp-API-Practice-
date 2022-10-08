3f4c77dae8d80dbf55509946af8ef23549b0254c
Victor Ponce <vicmanuelr@gmail.com>
2022-10-08 10:19:36 -0600

    All elements added to UI
    - on ui.py added al elements with tkinter
    - final version of UI elements and visual configurations

--- ui.py
+++ ui.py
@@ -11,7 +11,7 @@ class QuizzInterface:
         self.window.configure(bg=THEME_COLOR, padx=20, pady=20)
         # configure canvas
         self.canvas = Canvas(bg="white", width=300, height=250)
-        self.canvas.grid(column=0, row=1, columnspan=2)
+        self.canvas.grid(column=0, row=1, columnspan=2, padx=20, pady=20)
         self.question = self.canvas.create_text(150, 125, justify="center", font=("Arial", 20, "italic"),
                                                 text="Here goes the text")
         # buttons configuration
@@ -19,9 +19,9 @@ class QuizzInterface:
         self.false_img = PhotoImage(file="./images/false.png")
         self.true_button = Button(image=self.true_img)
         self.false_button = Button(image=self.false_img)
-        self.true_button.grid(column=0, row=2)
-        self.false_button.grid(column=1, row=2)
+        self.true_button.grid(column=0, row=2, padx=20, pady=20)
+        self.false_button.grid(column=1, row=2, padx=20, pady=20)
         # score tag
         self.score = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
-        self.score.grid(column=1, row=0)
+        self.score.grid(column=1, row=0, padx=20, pady=20)
         self.window.mainloop()
